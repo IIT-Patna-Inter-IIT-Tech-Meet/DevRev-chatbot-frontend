e6dadf9ddd1661c9156c99d88c370c930f4635ce
Anuag-Deo <anuragdeo03@gmail.com>
2023-12-09 18:08:47 +0530

    Added title, favicon and styling of scrollbar

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <link rel="icon" href="/icons.png" />
+        <meta name="description" content="Chatbot" />
+        <meta name="og:title" content="Chatbot" />
+        <title>ChatBot | Team 46</title>
+        </Head>
       <body>
         <Main />
         <NextScript />
